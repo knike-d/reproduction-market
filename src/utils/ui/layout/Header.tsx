d9663b791f4649d4e3754321f1df145e4acb0c63
knike-d <knike.develop@gmail.com>
2024-05-28 00:16:27 +0900

    refactor: ヘッダーのレイアウトをflexで制御する

--- src/utils/ui/layout/Header.tsx
+++ src/utils/ui/layout/Header.tsx
@@ -3,11 +3,11 @@ import Link from "next/link";
 export const Header = () => {
   return (
     <header className="bg-emerald-500">
-      <nav className="relative m-auto flex max-w-5xl p-3 pl-5 font-bold text-white">
-        <Link href="/" className="mr-5">
+      <nav className="flex h-12 max-w-5xl items-center font-bold text-white">
+        <Link href="/" className="mr-auto flex h-inherit items-center px-5">
           Home
         </Link>
-        <Link href="/category/" className="absolute right-4">
+        <Link href="/category" className="mr-4 flex h-inherit items-center">
           カテゴリ一覧
         </Link>
       </nav>
