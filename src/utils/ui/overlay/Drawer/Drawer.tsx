c79c6cb9ce00bb02ec95ecd1938033694950868e
knike-d <knike.develop@gmail.com>
2024-06-27 00:24:46 +0900

    refactor: drawerのoverlayをcomponentとして分離

--- src/utils/ui/overlay/Drawer/Drawer.tsx
+++ src/utils/ui/overlay/Drawer/Drawer.tsx
@@ -6,6 +6,7 @@ import { useFocusTrap } from "@/utils/hooks/accessibility/useFocusTrap.hook";
 import { useKeyEvent } from "@/utils/hooks/accessibility/useKeyEvent.hook";
 import { useToggleBodyFixed } from "@/utils/hooks/uiControl/useToggleBodyFixed";
 import { CloseIcon } from "@/utils/ui/Icon/CloseIcon";
+import { FixedOverlay } from "@/utils/ui/overlay/FixedOverlay";
 
 export const DrawerContext = createContext({
   isOpen: false,
@@ -31,11 +32,7 @@ export const Drawer = forwardRef(function Drawer(
 
   return (
     <>
-      <div
-        className={`fixed inset-0 z-overlay size-full overflow-hidden bg-overlay transition-opacity duration-300 ${isOpen ? "pointer-events-auto opacity-100" : "pointer-events-none opacity-0"}`}
-        aria-hidden
-        onClick={onClose}
-      />
+      <FixedOverlay isOpen={isOpen} onClose={onClose} />
       <div
         ref={drawerContentsRef}
         aria-hidden={!isOpen}
